faf2bae43ede11e51a50c2d5032720fffb957548
sanfusu <sanfusu@outlook.com>
2020-08-12 20:51:22 +0800

    trun into pub

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -10,14 +10,16 @@ pub fn as_slice_derive(input: TokenStream) -> TokenStream {
     let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
     let gen = quote! {
         impl #impl_generics #name #ty_generics #where_clause{
-            fn as_slice<'a>(&'a self)->&'a [u8]{
+            #[doc="类型转换"]
+            pub fn as_slice<'a>(&'a self)->&'a [u8]{
                 unsafe {
                     std::slice::from_raw_parts(
                         self as *const Self as *const u8, std::mem::size_of::<Self>()
                     )
                 }
             }
-            fn as_mut_slice<'a>(&'a mut self)->&'a mut [u8]{
+            #[doc="类型转换"]
+            pub fn as_mut_slice<'a>(&'a mut self)->&'a mut [u8]{
                 unsafe {
                     std::slice::from_raw_parts_mut(
                         self as *mut Self as *mut u8, std::mem::size_of::<Self>()
