66fe7742dc3e8d1e39b9f1ad7890e18185517578
sanfusu <sanfusu@foxmail.com>
2020-12-17 01:10:55 +0800

    fix doc

--- src/elf32/chunk.rs
+++ src/elf32/chunk.rs
@@ -19,8 +19,6 @@ use std::ops::{Deref, DerefMut};
 
 /// 用来表示 section 中的数据块，一个 section 可能会有多个数据块，
 /// 每一个数据块的大小由 [`Shdr::sh_entsize`](crate::elf32::section::Shdr::sh_entsize) 决定
-///
-/// 对 Vec<u8> 类型的简单封装，实现了 Deref。
 #[derive(Default)]
 pub struct DataChunk<T> {
     data: T,
