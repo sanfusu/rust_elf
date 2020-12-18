328dc64fd269fb5b101fe9dbb2c9f386d51b6c89
sanfusu <sanfusu@foxmail.com>
2020-12-18 12:24:55 +0800

    bug fix

--- src/lib.rs
+++ src/lib.rs
@@ -49,12 +49,14 @@ pub struct WrapperMut<'a, T> {
 /// let tmp = ['a' as u8,'b' as u8,'c' as u8,'\0' as u8];
 /// let str_tab = StrTab::new(&tmp);
 /// assert_eq!(&str_tab[0], "abc");
-/// assert_eq!(&str_tab[1],"bc");
+/// assert_eq!(&str_tab[1], "bc");
+/// assert_eq!(&str_tab[3], "");
 ///
 /// let tmp_witout_null = ['a' as u8,'b' as u8,'c' as u8];
 /// let str_tab_without_null = StrTab::new(&tmp);
 /// assert_eq!(&str_tab_without_null[0], "abc");
-/// assert_eq!(&str_tab_without_null[1],"bc");
+/// assert_eq!(&str_tab_without_null[1], "bc");
+/// assert_eq!(&str_tab_without_null[3], "");
 /// ```
 pub struct StrTab<'a> {
     src: &'a [u8],
@@ -70,12 +72,28 @@ impl Index<usize> for StrTab<'_> {
     type Output = str;
 
     fn index(&self, index: usize) -> &Self::Output {
-        let end = self
-            .src
-            .iter()
-            .enumerate()
-            .find(|x| *x.1 == 0)
-            .map_or(self.src.len(), |x| x.0);
-        std::str::from_utf8(&self.src[index..end]).map_or("", |x| x)
+        let src = &self.src[index..];
+        let end = src.iter().position(|&x| x == 0).unwrap_or(src.len());
+        unsafe { std::str::from_utf8_unchecked(&src[..end]) }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::StrTab;
+
+    #[test]
+    fn str_tab_test() {
+        let tmp = ['a' as u8, 'b' as u8, 'c' as u8, '\0' as u8];
+        let str_tab = StrTab::new(&tmp);
+        assert_eq!(&str_tab[0], "abc");
+        assert_eq!(&str_tab[1], "bc");
+        assert_eq!(&str_tab[3], "");
+
+        let tmp_witout_null = ['a' as u8, 'b' as u8, 'c' as u8];
+        let str_tab_without_null = StrTab::new(&tmp_witout_null);
+        assert_eq!(&str_tab_without_null[0], "abc");
+        assert_eq!(&str_tab_without_null[1], "bc");
+        assert_eq!(&str_tab_without_null[3], "");
     }
 }
