f8c3ba948db6cfbb103dcf4e9417c5eb697a928c
sanfusu <sanfusu@foxmail.com>
2020-12-19 21:41:57 +0800

    new proc_macro

---
+++ layout/README.md
@@ -0,0 +1,25 @@
+# 说明
+
+`Layout` proc_macro 用于为结构体生成各字段按 packed 表示在内存中的布局范围。如：
+
+```rust
+#[derive(Layout)]
+struct A {
+    a: u32;
+    b: u8;
+}
+```
+
+会生成：
+
+```rust
+struct ALayout;
+impl ALayout {
+    pub const fn a() -> std::ops::Range<usize> {
+        0..4 // 0, 1, 2, 3 号字节
+    }
+    pub const fn b() -> std::ops::Range<usize> {
+        4..5 // 4 号字节
+    }
+}
+```
---
+++ layout/src/lib.rs
@@ -0,0 +1,58 @@
+use proc_macro::TokenStream;
+use proc_macro2::{Ident, TokenStream as TokenStream2};
+use quote::{format_ident, quote};
+use syn::{parse2, parse_quote, Data, DeriveInput, Expr, Type};
+
+#[proc_macro_derive(Layout)]
+pub fn layout(input: TokenStream) -> TokenStream {
+    struct_layout(input.into()).into()
+}
+
+fn struct_layout(input: TokenStream2) -> TokenStream2 {
+    let ast: DeriveInput = parse2(input).unwrap();
+    let ident = ast.ident;
+    let layout_struct = format_ident!("{}Layout", ident);
+    let mut layout_ident: Vec<Ident> = Vec::new();
+    let mut layout_offset_after: Vec<Expr> = Vec::new();
+    let mut layout_offset_prev: Vec<Expr> = Vec::new();
+    layout_offset_prev.push(parse_quote! {0});
+
+    if let Data::Struct(data) = ast.data {
+        data.fields.iter().for_each(|x| {
+            let ty: Type = x.ty.clone();
+            let current_size: Expr = parse_quote! {std::mem::size_of::<#ty>()};
+            let current_offset: Expr;
+
+            match layout_offset_after.last() {
+                Some(lo) => {
+                    current_offset = parse_quote! { #lo + #current_size};
+                }
+                None => {
+                    current_offset = parse_quote! {#current_size};
+                }
+            }
+            layout_offset_after.push(current_offset);
+            layout_ident.push(x.clone().ident.expect("Should be all named field"));
+        })
+    } else {
+        panic!("We need struct");
+    }
+
+    layout_offset_after
+        .iter()
+        .for_each(|x| layout_offset_prev.push(x.clone()));
+    layout_offset_prev.pop();
+    let usage_msg = format!(
+        "用于获取 {} 每个字段的内存布局，如 `{}::{}();`",
+        ident.to_string(),
+        layout_struct.to_string(),
+        layout_ident[0].to_string()
+    );
+    quote! {
+        #[doc=#usage_msg]
+        pub struct #layout_struct;
+        impl #layout_struct {
+            #(pub const fn #layout_ident()->std::ops::RangeInclusive<usize> { #layout_offset_prev..=#layout_offset_after})*
+        }
+    }
+}
